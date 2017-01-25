e4bf38ce693c5106dfee3e188271b7da08e25177
jojoldu <jojoldu@gmail.com>
2017-01-25 23:50:47 +0900

    [0.0.8] blog.json, token.json 위치를 홈 디렉토리로 변경 (update, install 마다 init하는 불편함때문에 외부로 이동)

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jojoldu@gmail.com on 2017-01-25
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+
+const myPath = require('../lib/my-path');
+
+console.log(myPath.getMyRootPath());
