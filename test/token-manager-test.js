e6a56fb40faadef0da13a68e87a85f232f4800fe
이동욱 <jojoldu@gmail.com>
2018-09-16 22:00:47 +0900

    token-manager.js async-await로 변경

--- test/token-manager-test.js
+++ test/token-manager-test.js
@@ -4,13 +4,13 @@
  * Github : http://github.com/jojoldu
  */
 
-const tokenGenerator = require('../lib/token-manager');
+const tokenManager = require('../lib/token-manager');
 const assert = require('assert');
 
 describe('token-generator', ()=>{
     describe('Root Path에 있는 blog정보의 타이틀은 jojoldu이다.', ()=>{
         it('getBlogJson은 Promise 객체 리턴한다.', (done)=>{
-            tokenGenerator.getBlogJson()
+            tokenManager.getBlogJson()
                 .then((json)=>{
                     assert.equal(json.blogName, 'jojoldu');
                     done();
