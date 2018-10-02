661e980c9b4654c73e234524fef125def9b0b3d1
HardPlant <abc7988se@naver.com>
2018-10-03 00:14:55 +0900

    writelastest 명령 추가

--- test/file-finder-test.js
+++ test/file-finder-test.js
@@ -6,6 +6,7 @@
 
 const fileFinder = require('../lib/file-finder');
 const assert  = require('assert');
+const fs = require('fs')
 
 describe('file-finder 테스트', () => {
     describe('실행위치에서 파일 찾기', () => {
@@ -25,6 +26,24 @@ describe('file-finder 테스트', () => {
                     assert.equal(markdownFile.title, 'PARSER');
                     done();
                 });
+        });
+    });
+    describe('상대적으로 새로운 파일 찾기', ()=>{
+        before('test 실행 위치에서 실행되므로, \
+            test의 Z_Newest_File.md를 test 실행 위치로 옮긴다',()=>{
+                fs.createReadStream('README.md').pipe(fs.createWriteStream('newREADME.md'));
+            });
+        
+        it('새로 복사된 newREADME를 찾는다', (done) => {
+            fileFinder.findMarkdownFileNewest()
+                .then((markdownFile) => {
+                    assert.equal(markdownFile.title, 'newREADME');
+                    done();
+            });
+        });
+
+        after('완료 후 복사한 RENAME.md를 지운다', ()=>{
+            fs.unlinkSync('newREADME.md');
         });
     });
 });
