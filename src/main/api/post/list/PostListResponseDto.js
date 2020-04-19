7dfde1369ab73adf30eca47d9e58bbcb01751546
이동욱 <jojoldu@gmail.com>
2020-04-19 22:55:22 +0900

    Post List Response Dto 응답결과 테스트

---
+++ src/main/api/post/list/PostListResponseDto.js
@@ -0,0 +1,42 @@
+/**
+ *
+ {
+  "tistory": {
+    "status": "200",
+    "item": {
+      "url": "http://oauth-test.tistory.com",
+      "secondaryUrl": "",
+      "page": "1",
+      "count": "10",
+      "totalCount": "181",
+      "posts": [
+        {
+          "id": "201",
+          "title": "테스트 입니다.",
+          "postUrl": "http://oauth-test.tistory.com/201",
+          "visibility": "0",
+          "categoryId": "0",
+          "comments": "0",
+          "trackbacks": "0",
+          "date": "2018-06-01 17:54:28"
+        },
+        ...
+      ]
+    }
+  }
+}
+ * @type {PostListResponseDto}
+ */
+module.exports = class PostListResponseDto {
+    constructor(tistory) {
+        this.tistory = tistory;
+    }
+
+    isNotOk () {
+        return !this.isOk();
+    }
+
+    isOk () {
+        return this.status === "200";
+    }
+}
---
+++ src/test/api/post/list/PostListResponseDto.test.js
@@ -0,0 +1,31 @@
+const PostListResponseDto = require('src/main/api/post/list/PostListResponseDto');
+
+test("응답 객체의 상태를 dto로 변환한다", () => {
+    const apiResponse = {
+        "tistory": {
+            "status": "200",
+            "item": {
+                "url": "http://oauth-test.tistory.com",
+                "secondaryUrl": "",
+                "page": "1",
+                "count": "10",
+                "totalCount": "181",
+                "posts": [
+                    {
+                        "id": "201",
+                        "title": "테스트 입니다.",
+                        "postUrl": "http://oauth-test.tistory.com/201",
+                        "visibility": "0",
+                        "categoryId": "0",
+                        "comments": "0",
+                        "trackbacks": "0",
+                        "date": "2018-06-01 17:54:28"
+                    },
+                ]
+            }
+        }
+    };
+
+    const dto = new PostListResponseDto(apiResponse);
+    expect(dto.isNotOk()).toBe(true);
+})
